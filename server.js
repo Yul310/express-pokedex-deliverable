1a7e7ecd20ff0f93cdb5a4747fe8ba6c64e55977
Yul310 <hsy3102@gmail.com>
2022-06-06 22:58:03 -0400

    Initiation

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = require("liquid-express-views")(express())
+const methodOverride = require('method-override')
+const pokemons = require('./models/pokedex/pokemon.js')
+
+
+app.listen(3000,() => {
+    
+    console.log("working")
+
+})
+
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+
+app.use(express.urlencoded({
+    extended: false
+})) // allows us to view body of a post request
+
+app.use(express.static('public')); //tells express to try to match requests with files in the directory called 'public'
+app.use(express.json()) // This prepares our api to receive json data from the body of all incoming requests.
+//use methodOverride. We'll be adding a query parameter to our delete form named_method
+app.use(methodOverride('_method'))
+
+
+
+
+app.get('/pokedex',(req,res) =>{
+    // res.send("hello")
+    res.render(
+        'index',{
+            allPokemons: pokemons
+        }
+    )
+})
